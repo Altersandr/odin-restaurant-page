01e7c029b7dc7f3adf8c2efb9f191713c9052a65
Altersandr <114981940+Altersandr@users.noreply.github.com>
2022-12-10 17:45:22 +0100

    Update menu.js

--- src/scripts/menu.js
+++ src/scripts/menu.js
@@ -2,63 +2,63 @@
 const item1 = {
     name: "item1",
     title: "Roses Mille Crepe",
-    pic: "/src/img/rosesmillecrepe.jpg",
+    pic: "src/img/rosesmillecrepe.jpg",
     price: "6€"
 };
 
 const item2 = {
     name: "item2",
     title: "Dorayaki",
-    pic: "/src/img/dorayaki.jpg",
+    pic: "src/img/dorayaki.jpg",
     price: "3€"
 };
 
 const item3 = {
     name: "item3",
     title: "Magnum Pan",
-    pic: "/src/img/magnumpan.jpg",
+    pic: "src/img/magnumpan.jpg",
     price: "7€"
 };
 
 const item4 = {
     name: "item4",
     title: "Creepy Cupcake",
-    pic: "/src/img/creepycupcake.jpg",
+    pic: "src/img/creepycupcake.jpg",
     price: "3,5€"
 };
 
 const item5 = {
     name: "item5",
     title: "Pikachu",
-    pic: "/src/img/pikachu.jpg",
+    pic: "src/img/pikachu.jpg",
     price: "5,5€"
 };
 
 const item6 = {
     name: "item6",
     title: "Mooncake",
-    pic: "/src/img/mooncake.jpg",
+    pic: "src/img/mooncake.jpg",
     price: "4€"
 };
 
 const item7 = {
     name: "item7",
     title: "Caramel Mille Crepe",
-    pic: "/src/img/caramelmillecrepe.jpg",
+    pic: "src/img/caramelmillecrepe.jpg",
     price: "6€",
 };
 
 const item8 = {
     name: "item8",
     title: "Melon Pan",
-    pic: "/src/img/melonpan.jpg",
+    pic: "src/img/melonpan.jpg",
     price: "5€"
 };
 
 const item9 = {
     name: "item9",
     title: "Hokkaido Milk Toast",
-    pic: "/src/img/hokkaidomilktoast.jpg",
+    pic: "src/img/hokkaidomilktoast.jpg",
     price: "4,5€"
 };
 
